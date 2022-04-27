5267d5243a1fedadf91c6773191947a52848cd79
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-04-27 17:21:50 +0300

    task12

--- server.js
+++ server.js
@@ -1,52 +1,119 @@
 'use strict';
-const express1 = require('express');
-const recData=require("./data.json");
-function Data(title,path,overview)
-{
-    this.title=title;
-    this.path=path;
-    this.overview=overview;
-}
+//2f7c43dfa0698bbdddc7090cdc618d03
 
-const app1 = express1();
+const recData = require("./data.json");
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios').default;
+//require('dotenv').config()
 const port = 3000;
-app1.listen(port, () => {
-  //console.log(recData);
-  })
+
+const app1 = express();
+app1.use(cors());
+
+// const apiKey = process.env.API_KEY;
 
 
-app1.get("/",handleHome);
-function handleHome(req,res)
-{
-   let result =new Data( recData.title,recData.poster_path,recData.overview);
-  //let result =new Data( "yes","yes","yes");
-   res.json(result);
-  
+
+function Data(title, path, overview) {
+  this.title = title;
+  this.path = path;
+  this.overview = overview;
 }
 
-app1.get("/fav",handleFav);
-function handleFav(req,res)
-{
+
+
+app1.listen(port, () => {
+  
+})
+
+
+app1.get("/", handleHome);
+function handleHome(req, res) {
+  let result = new Data(recData.title, recData.poster_path, recData.overview);
+  res.json(result);
+
+}
+
+function Movie(id, title, release_date, poster_path, overview) {
+  this.id = id;
+  this.title = title;
+  this.release_date = release_date;
+  this.poster_path = poster_path;
+  this.overview = overview;
+}
+//trending
+app1.get("/trending", handleTrend);
+function handleTrend(req, res) {
+   const url = `http://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US?api_key=2f7c43dfa0698bbdddc7090cdc618d03`;
+  axios.get(url).then(result => {
+  // console.log("array"+result.data);
+    let moveis = result.data.results.map(result => {
+
+      return new Movie(result.id, result.title, result.release_date, result.poster_path, result.overview);
+
+    })
+    res.json(moveis);
+  }).catch(error => { res.send("Inside catch") });
+}
+
+//search
+  app1.get("/search",searchHandling);
+  function searchHandling(req,res)
+  {
+    let movieName=req.query.name;
+    const url = `http://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US?api_key=2f7c43dfa0698bbdddc7090cdc618d03`;
+    axios.get(url)
+            .then(result => {
+               console.log("inside then search");
+                res.json(result.data.results)
+            })
+            .catch();
+        
+
+
+  }
+
+/**
+ * // function handleSearch(req, res) {
+//     // console.log(req.query);
+//     let recipeName = req.query.name; // I chose to call it name
+//     let url = `https://api.spoonacular.com/recipes/complexSearch?query=${recipeName}&apiKey=${apiKey}`
+//     axios.get(url)
+//         .then(result => {
+//             // console.log(result.data.results);
+//             res.json(result.data.results)
+//         })
+//         .catch();
+//     // waiting to get data from 3rd API
+//     // res.send("Searching for recipes");
+// }
+ */
+
+
+
+app1.get("/fav", handleFav);
+function handleFav(req, res) {
   res.send("Welcome to Favorite Page");
 }
 app1.use(handleErr500);
-function handleErr500 (req,res){
-let error ={
-  "status": 500,
-  "responseText": "Sorry, something went wrong"
+function handleErr500(req, res) {
+  let error = {
+    "status": 500,
+    "responseText": "Sorry, something went wrong"
   }
- 
+
   res.status(500).json(error);
-  
+
 }
 
-app1.get("*",handleErr404);
-function handleErr404 (req,res){
-let error ={
-  "status": 404,
-  "responseText": "page not found error"
+app1.get("*", handleErr404);
+function handleErr404(req, res) {
+  let error = {
+    "status": 404,
+    "responseText": "page not found error"
   }
   res.status(404).json(error);
-  
+
 }
 
